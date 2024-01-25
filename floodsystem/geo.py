2fc5d3919abd50c61376d962917746f7502fdec7
Yiqian Liang <yl992@cam.ac.uk>
2024-01-25 22:08:53 +0000

    TASK1C AND geo

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,30 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+#Task1B
+from floodsystem.stationdata import build_station_list
+from haversine import haversine, Unit
+Stations = build_station_list()
+def stations_by_distance(stations, p):
+    list=[]
+    for station in stations:
+        distance=haversine(station.coord, p)
+        touple=(station.name,station.town,distance)
+        list.append(touple)
+    sorted_list=sorted(list, key=lambda x: x[-1])
+    return sorted_list
+
+#Task1C
+from floodsystem.stationdata import build_station_list
+from haversine import haversine, Unit
+Stations = build_station_list()
+def stations_within_radius(stations, centre, r):
+    list=[]
+    for station in stations:
+        distance=haversine(station.coord, centre)
+        if distance<r:
+            list.append(station.name)
+    list=sorted(list, key=lambda x:x[0])
+
+    return list
+

8e5e2592216af6f7cef13158566ad4685b91d196
carolxie0214 <tx239@cam.ac.uk>
2024-01-28 22:18:51 +0000

    done 2

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -37,40 +37,39 @@ def stations_within_radius(stations, centre, r):
 
 #Task1D
 from floodsystem.stationdata import build_station_list
-Stations = MonitoringStation()
+
+data = build_station_list()
 
 def rivers_with_station(stations):
     list_of_monitored_rivers=[]
     for station in stations:
-        if (station.name in list_of_monitored_rivers):
-            pass
-        else:
-            list_of_monitored_rivers.append(station.name)
-    return list_of_monitored_rivers
+        list_of_monitored_rivers.append(station.river)
+    set_of_monitored_rivers=set(list_of_monitored_rivers)
+    return set_of_monitored_rivers
 
 def stations_by_river(stations):
     dic = dict()
-    for i in len(stations):
-        dic[stations[i].name] = ''
+    for river in rivers_with_station(stations):
+        dic[river] = []
+    
+    for station in stations:
+        dic[station.river].append(station)
     return dic
 
 #Task1E
-from floodsystem.stationdata import build_station_list
-from floodsystem.geo import stations_by_river
-
 def rivers_by_station_number(stations, N):
-    dic_river = stations_by_river(stations)
+    dic_river = {key: len(value) for key, value in stations_by_river(stations).items()}
+    my_list = [(k, v) for k, v in dic_river.items()]
+    sorted_list = sorted(my_list, key=lambda t:t[1])
+    sorted_list.reverse()
+    selected_list_initial = sorted_list[:N]
 
-    my_list = list[zip(dic_river.keys(),dic_river.values())]
-    sorted_my_list =sorted(my_list, key=lambda x:(x[1],x[0]))
-    selected_list_initial = sorted_my_list[0,N]
-    for i in range(N+1, len(sorted_my_list)):
-        if sorted_my_list[i][1] == sorted_my_list[N][1]:
-            selected_list_initial.append(sorted_my_list[i])
+    for i in range(N+1, len(sorted_list)):
+        if sorted_list[i][1] == sorted_list[N][1]:
+            selected_list_initial.append(sorted_list[i])
         else:
             break
-    print(selected_list_initial)
-
+    return selected_list_initial
 
 
 

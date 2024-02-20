bdc9feba2fcaa84feeb8ce244a8456a2babf9ab6
Yiqian-Liang <147544143+Yiqian-Liang@users.noreply.github.com>
2024-02-20 19:22:06 +0000

    Change the output form of task2B

--- Task2B.py
+++ Task2B.py
@@ -1,4 +1,6 @@
 from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list
 Stations=build_station_list()
-print(stations_level_over_threshold(Stations, 0.8))
+R=stations_level_over_threshold(Stations, 0.8)
+for i in range(len(R)):
+    print(R[i][0],R[i][1])

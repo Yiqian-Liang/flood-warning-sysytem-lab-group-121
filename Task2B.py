15def442fb83b09f9aba07c0d87b4145c46f22f7
Yiqian-Liang <147544143+Yiqian-Liang@users.noreply.github.com>
2024-02-20 17:11:54 +0000

    Delete Task2B.py

--- Task2B.py
+++
@@ -1,4 +0,0 @@
-from floodsystem.flood import stations_level_over_threshold
-from floodsystem.stationdata import build_station_list
-Stations=build_station_list()
-print(stations_level_over_threshold(Stations,0.8))
